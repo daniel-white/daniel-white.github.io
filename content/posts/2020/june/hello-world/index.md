61c308208bd0573ff3676e701338ea6c4b46cf82
Daniel A. White <daniel@whitefamily.in>
2020-06-28 20:05:54 -0400

    seeding gatsby

---
+++ content/posts/2020/june/hello-world/index.md
@@ -0,0 +1,8 @@
+---
+title: Hello World
+date: "2020-06-28"
+description: "Getting started with Gatsby"
+slug: "blog/2020/june/hello-world"
+---
+
+I'm building a new home here with Gatsby. Please standby.
---
+++ gatsby-config.js
@@ -0,0 +1,77 @@
+require(`dotenv`).config({
+  path: `.env`,
+})
+
+module.exports = {
+  siteMetadata: {
+    siteTitle: `Daniel A. White`,
+    author: `Daniel A. White`,
+    siteHeadline: `Passion-driven software engineering`,
+    siteTitleAlt: `Daniel A. White's Blog`,
+    siteDescription: `Daniel A. White's Blog`,
+    siteUrl: `https://daniel-white.github.io/`,
+    siteLanguage: `en`,
+    siteImage: ``,
+  },
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-minimal-blog`,
+      // See the theme's README for all available options
+      options: {
+        navigation: [
+          /*{
+            title: `Blog`,
+            slug: `/blog`,
+          },*/
+          {
+            title: `About`,
+            slug: `/about`,
+          },
+        ],
+        externalLinks: [
+          {
+            name: `Stack Overflow`,
+            url: `https://stackoverflow.com/users/23528/daniel-a-white`,
+          },
+          {
+            name: `Twitter`,
+            url: `https://twitter.com/thetrueaplus`,
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+      },
+    },
+    `gatsby-plugin-sitemap`,
+    /*{
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
+        short_name: `minimal-blog`,
+        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#6B46C1`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },*/
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify`,
+  ],
+}
