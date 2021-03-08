e60b3d4cfcf55a4c2f6bd4603f300d87a80a9bbe
Daniel A. White <daniel@whitefamily.in>
2021-03-07 22:10:45 -0500

    install prettier (#118)

--- src/components/reputation.tsx
+++ src/components/reputation.tsx
@@ -1,39 +1,44 @@
-import React from 'react';
-import axios from 'axios';
-import adapter from 'axios-jsonp';
-import isBrowserHook from '../hooks/isBrowser';
+import React from "react";
+import axios from "axios";
+import adapter from "axios-jsonp";
+import isBrowserHook from "../hooks/isBrowser";
 
 class ReputationBrowser extends React.Component<{}, { reputation?: number }> {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-    async componentDidMount() {
-        const response = await axios({
-            url: 'https://api.stackexchange.com/2.2/users/23528?&site=stackoverflow',
-            adapter
-        });
-        if (response.status === 200) {
-            const userDetails = response.data.items[0];
-            const reputation = userDetails.reputation;
-            this.setState({
-                reputation
-            });
-        }
+  async componentDidMount() {
+    const response = await axios({
+      url: "https://api.stackexchange.com/2.2/users/23528?&site=stackoverflow",
+      adapter,
+    });
+    if (response.status === 200) {
+      const userDetails = response.data.items[0];
+      const reputation = userDetails.reputation;
+      this.setState({
+        reputation,
+      });
     }
+  }
 
-    render() {
-        return <span>{ this.state.reputation?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }</span>
-    }
+  render() {
+    return (
+      <span>
+        {this.state.reputation
+          ?.toString()
+          .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+      </span>
+    );
+  }
 }
 
 export default function Reputation() {
-    const isBrowser = isBrowserHook();
-    if (isBrowser) {
-        return <ReputationBrowser />
-    }
-    else {
-        return null;
-    }
+  const isBrowser = isBrowserHook();
+  if (isBrowser) {
+    return <ReputationBrowser />;
+  } else {
+    return null;
+  }
 }
